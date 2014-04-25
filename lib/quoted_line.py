0c9004a2d0d77c4adba41d1aa21324dd2a90f077
Chris <cidzerda@sbcglobal.net>
2014-04-25 12:48:38 -0700

    Reformatted a Python script.

--- lib/quoted_line.py
+++ lib/quoted_line.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
 def parse(line, delimiter):
-	line = line.strip()
-	if '"' in line:
-		in_quotes = False
-		parts = []
-		part = ""
-		for ch in line:
-			if in_quotes:
-				if ch == '"':
-					in_quotes = False
-				else:
-					part += ch
-			else:
-				if ch == '"':
-					in_quotes = True
-				elif ch == delimiter:
-					parts.append(part)
-					part = ""
-				else:
-					part += ch
-		parts.append(part)
-		return parts
-	else:
-		return line.split(delimiter)
+  line = line.strip()
+  if '"' in line:
+    in_quotes = False
+    parts = []
+    part = ""
+    for ch in line:
+      if in_quotes:
+        if ch == '"':
+          in_quotes = False
+        else:
+          part += ch
+      else:
+        if ch == '"':
+          in_quotes = True
+        elif ch == delimiter:
+          parts.append(part)
+          part = ""
+        else:
+          part += ch
+    parts.append(part)
+    return parts
+  else:
+    return line.split(delimiter)
