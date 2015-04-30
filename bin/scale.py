c5ca184de7922b2719e5ec90196003452c664eec
Chris <cidzerda@sbcglobal.net>
2015-04-30 11:40:17 -0700

    Allow empty values and clamping when scaling.
    Use the media value for empty values.

--- bin/scale.py
+++ bin/scale.py
@@ -1,34 +1,84 @@
 #!/usr/bin/env python3
 
 import argparse
+import math
 import sys
 import tempfile
 
+nan= float("NaN")
+
 class MinMax:
   def __init__(self, value):
-    parts= value.strip().split()
-    self.offset= float(parts[0])
-    self.maximum= float(parts[-1])
-    self.factor= self.maximum - self.offset
+    self.wants_clamping= False
+    if ' ' in value:
+      self.is_restoring= True
+      parts= value.strip().split()
+      self.minimum, self.maximum, self.middle= map(float, parts)
+      self.divisor= self.maximum - self.minimum
+    else:
+      self.is_restoring= False
+      self.divisor= .0
+      self.middle= .5
+      try:
+        value= value.strip()
+        self.minimum= self.maximum= float(value)
+      except ValueError as ex:
+        # Ignore non-values.
+        if value == '' or value == 'None':
+          self.minimum= self.maximum= nan
+        else:
+          raise ex
 
   def add(self, value):
-    value= float(value)
-    self.offset= min(self.offset, value)
-    self.maximum= max(self.maximum, value)
-    self.factor= self.maximum - self.offset
+    if self.is_restoring:
+      raise Exception("cannot add a value when restoring")
+    try:
+      value= value.strip()
+      value= float(value)
+    except ValueError as ex:
+      # Ignore non-values.
+      if value == '' or value == 'None':
+        return
+      else:
+        raise ex
+    self.minimum= min(value, self.minimum)
+    self.maximum= max(value, self.maximum)
+    self.divisor= self.maximum - self.minimum
+    if math.isnan(self.divisor):
+      self.divisor= .0
 
   def __repr__(self):
-    return "{} {}".format(self.offset, self.maximum)
+    return "{} {} {}".format(self.minimum, self.maximum, self.middle)
 
   def adjust(self, value):
-    return (float(value) - self.offset) / self.factor if self.factor else float(value)
+    try:
+      value= value.strip()
+      result= (float(value) - self.minimum) / self.divisor if self.divisor else self.middle
+      if self.wants_clamping:
+        result= min(max(0 if self.middle else -1, result), 1)
+      return result
+    except ValueError as ex:
+      if value == '' or value == 'None':
+        return self.middle
+      else:
+        raise ex
 
   def ignore(self):
-    self.offset= self.maximum= self.factor= 0
+    if self.is_restoring:
+      raise Exception("cannot ignore when restoring")
+    self.minimum= self.maximum= self.divisor= 0
+
+  def use_clamping(self):
+    if not self.is_restoring:
+      raise Exception("cannot clamp when saving")
+    self.wants_clamping= True
 
   def use_negative_one(self):
-    self.factor /= 2
-    self.offset= self.maximum - self.factor
+    if self.is_restoring:
+      raise Exception("cannot change lower bound when restoring")
+    self.middle= .0
+    self.divisor /= 2
+    self.minimum= self.maximum - self.divisor
 
 class MinMaxDict:
   def __init__(self):
@@ -98,23 +148,29 @@ def save(settings_file, input_file, output_file, columns_to_ignore, using_negati
   input_file.seek(0)
   normalize(minmax_dict, input_file, output_file)
 
-def restore(settings_file, input_file, output_file):
+def restore(settings_file, input_file, output_file, wants_clamping):
   g= (line.split(None, 1) for line in settings_file)
   minmax_dict= {int(p[0]): MinMax(p[1]) for p in g}
+  if wants_clamping:
+    for v in minmax_dict.values():
+      v.use_clamping()
   normalize(minmax_dict, input_file, output_file)
 
 if __name__ == "__main__":
-  parser= argparse.ArgumentParser(description="""Scales a CSV dataset so
-    all features are in [0, 1].""", epilog="""You must specify either save
-    (-s, --save) or restore (-r, --restore).  The restore operation ignores
-    the negative-one and the ignore parameters.  The save operation stores
-    information about those parameters in the settings file.""")
+  parser= argparse.ArgumentParser(description="""Scales a CSV dataset so all
+    features are in [0, 1].""", epilog="""You must specify either save (-s,
+    --save) or restore (-r, --restore).  The restore operation ignores the
+    negative-one and ignore options.  The save operation stores information
+    about those options in the settings file.  Only the restore operation uses
+    the clamp option.""")
   parser.add_argument("-s", "--save", metavar="settings_file",
     type=argparse.FileType('w'),
     help="the scaling parameter settings save file")
   parser.add_argument("-r", "--restore", metavar="settings_file",
     type=argparse.FileType('r'),
     help="the scaling parameter settings restore file")
+  parser.add_argument("-c", "--clamp", action="store_true",
+    help="ensure scaled values are within range")
   parser.add_argument("-n", "--negative-one", action="store_true",
     help="use -1 instead of 0 as the lower bound")
   parser.add_argument("-i", "--ignore", metavar="COLUMNS", default="",
@@ -129,6 +185,6 @@ if __name__ == "__main__":
   if args.save and not args.restore:
     save(args.save, args.input_file, args.output_file, args.ignore, args.negative_one)
   elif args.restore and not args.save:
-    restore(args.restore, args.input_file, args.output_file)
+    restore(args.restore, args.input_file, args.output_file, args.clamp)
   else:
     parser.parse_args(["-h"])
