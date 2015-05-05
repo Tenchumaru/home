6d873a657b6cb2002795009713b3616c01aae711
Chris <cidzerda@sbcglobal.net>
2015-05-05 09:08:20 -0700

    Implemented optionally skipping the BOM.

--- src/as-ascii.c
+++ src/as-ascii.c
@@ -5,7 +5,8 @@
 
 static int usage(char const* prog)
 {
-	fprintf(stderr, "usage: %s [-h] [-u] [input_file [output_file]]\n", prog);
+	fprintf(stderr, "usage: %s [-h] [-b] [-u] [input_file [output_file]]\n", prog);
+	fputs("use -b to remove BOM from input\n", stderr);
 	fputs("use -u to strip CRs from input\n", stderr);
 	return 2;
 }
@@ -13,13 +14,16 @@ static int usage(char const* prog)
 int main(int argc, char* argv[])
 {
 	char const* prog= argv[0];
-	bool strip_cr= false;
+	bool remove_bom= false, strip_cr= false;
 	while(argc > 1 && argv[1][0] == '-' && argv[1][1])
 	{
 		switch(argv[1][1])
 		{
 		case 'h':
 			return usage(prog);
+		case 'b':
+			remove_bom= true;
+			break;
 		case 'u':
 			strip_cr= true;
 			break;
@@ -51,6 +55,17 @@ int main(int argc, char* argv[])
 		}
 	}
 	int ch;
+	if(remove_bom)
+	{
+		ch= getc(fin);
+		if(ch == 0xef)
+		{
+			getc(fin);
+			getc(fin);
+		}
+		else if(!strip_cr || ch != '\r')
+			putc(ch & 0x7f, fout);
+	}
 	while(ch= getc(fin), ch != EOF)
 	{
 		if(!strip_cr || ch != '\r')
