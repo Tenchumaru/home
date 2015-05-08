7b5fe9e494493628e5ef26f8da5bfb679b2397d1
Chris <cidzerda@sbcglobal.net>
2015-05-08 11:47:48 -0700

    Added replacement options.
    Implemented specifying a replacement character and ensuring all
    characters are printable.

--- src/as-ascii.c
+++ src/as-ascii.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define arg_is_dash(n) (argv[n][0] == '-' && !argv[n][1])
 
 static int usage(char const* prog)
 {
-	fprintf(stderr, "usage: %s [-h] [-b] [-u] [input_file [output_file]]\n", prog);
+	fprintf(stderr, "usage: %s [-h] [-b] [-p] [-r ch] [-u] [input_file [output_file]]\n", prog);
 	fputs("use -b to remove BOM from input\n", stderr);
+	fputs("use -p to ensure all characters are printable\n", stderr);
+	fputs("use -r to specify a replacement character\n", stderr);
 	fputs("use -u to strip CRs from input\n", stderr);
 	return 2;
+}
+
+static int replacement_ch= EOF;
+static bool strip_cr= false, wants_printable= false;
+
+static void print(int ch, FILE* fout)
+{
+	if(!strip_cr || ch != '\r')
+	{
+		if(replacement_ch == EOF)
+			ch &= 0x7f;
+		else if(ch & ~0x7f)
+			ch= replacement_ch;
+		if(wants_printable)
+		{
+			if(ch < ' ' && (!ch || !strchr("\t\n\v\r", ch)))
+				ch= replacement_ch != EOF ? replacement_ch : ' ';
+			else if('~' < ch)
+				ch= replacement_ch != EOF ? replacement_ch : '~';
+		}
+		putc(ch, fout);
+	}
 }
 
 int main(int argc, char* argv[])
 {
 	char const* prog= argv[0];
-	bool remove_bom= false, strip_cr= false;
+	bool remove_bom= false;
 	while(argc > 1 && argv[1][0] == '-' && argv[1][1])
 	{
 		switch(argv[1][1])
@@ -23,6 +48,15 @@ int main(int argc, char* argv[])
 			return usage(prog);
 		case 'b':
 			remove_bom= true;
+			break;
+		case 'p':
+			wants_printable= true;
+			break;
+		case 'r':
+			if(argc < 3)
+				return usage(prog);
+			replacement_ch= argv[2][0];
+			--argc, ++argv;
 			break;
 		case 'u':
 			strip_cr= true;
@@ -63,14 +97,11 @@ int main(int argc, char* argv[])
 			getc(fin);
 			getc(fin);
 		}
-		else if(!strip_cr || ch != '\r')
-			putc(ch & 0x7f, fout);
+		else
+			print(ch, fout);
 	}
 	while(ch= getc(fin), ch != EOF)
-	{
-		if(!strip_cr || ch != '\r')
-			putc(ch & 0x7f, fout);
-	}
+		print(ch, fout);
 	if(argc > 1)
 	{
 		fclose(fin);
