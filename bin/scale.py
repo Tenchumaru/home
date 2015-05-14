53ac43511a1216601cca3d2acbab6e151ff10898
Chris <cidzerda@sbcglobal.net>
2015-05-13 23:11:15 -0700

    Implemented scaling to the standard deviation.

--- bin/scale.py
+++ bin/scale.py
@@ -1,112 +1,109 @@
 #!/usr/bin/env python3
 
 import argparse
-import math
 import sys
 import tempfile
+from collections import defaultdict
+from math import isnan, sqrt
 
 nan= float("NaN")
 
 class MinMax:
-  def __init__(self, value):
-    self.wants_clamping= False
-    if ' ' in value:
-      self.is_restoring= True
-      parts= value.strip().split()
-      self.minimum, self.maximum, self.middle= map(float, parts)
-      self.divisor= self.maximum - self.minimum
+  def __init__(self, using_negative_one, using_standard_deviation):
+    if type(using_negative_one) == str:
+      parts= using_negative_one.split()
+      self.m, self.b, self.lower_bound, self.upper_bound= map(float, parts)
+      self.wants_clamping= using_standard_deviation
     else:
-      self.is_restoring= False
-      self.divisor= .0
-      self.middle= .5
-      try:
-        value= value.strip()
-        self.minimum= self.maximum= float(value)
-      except ValueError as ex:
-        # Ignore non-values.
-        if value == '' or value == 'None':
-          self.minimum= self.maximum= nan
-        else:
-          raise ex
+      self.upper_bound= 1.
+      self.lower_bound= -1. if using_negative_one else .0
+      self.middle= (self.upper_bound - self.lower_bound) / 2.
+      self.using_standard_deviation= using_standard_deviation
+      self.wants_clamping= False
+      if using_standard_deviation:
+        self.k= 0
+        self.a= self.q= .0
+      else:
+        self.minimum= self.maximum= nan
+
+  def __str__(self):
+    if self.using_standard_deviation and self.k != 0:
+      sd= sqrt(self.q / self.k)
+      self.minimum= self.a - sd
+      self.maximum= self.a + sd
+    if self.maximum == self.minimum or isnan(self.maximum) or isnan(self.minimum):
+      self.m= .0
+      self.b= (self.upper_bound - self.lower_bound) / 2.
+    else:
+      self.m= (self.upper_bound - self.lower_bound) / (self.maximum - self.minimum)
+      self.b= self.upper_bound - self.m * self.maximum
+    return '%g %g %g %g' % (self.m, self.b, self.lower_bound, self.upper_bound)
 
   def add(self, value):
-    if self.is_restoring:
-      raise Exception("cannot add a value when restoring")
     try:
       value= value.strip()
       value= float(value)
+      if isnan(value):
+        return
     except ValueError as ex:
       # Ignore non-values.
       if value == '' or value == 'None':
         return
       else:
         raise ex
-    self.minimum= min(value, self.minimum)
-    self.maximum= max(value, self.maximum)
-    self.divisor= self.maximum - self.minimum
-    if math.isnan(self.divisor):
-      self.divisor= .0
-
-  def __repr__(self):
-    return "{} {} {}".format(self.minimum, self.maximum, self.middle)
+    if self.using_standard_deviation:
+      self.k += 1
+      diff= value - self.a
+      self.a += diff / self.k
+      self.q += diff * (value - self.a)
+    else:
+      self.minimum= min(value, self.minimum)
+      self.maximum= max(value, self.maximum)
 
   def adjust(self, value):
     try:
       value= value.strip()
-      result= (float(value) - self.minimum) / self.divisor if self.divisor else self.middle
+      result= self.m * float(value) + self.b
       if self.wants_clamping:
-        result= min(max(0 if self.middle else -1, result), 1)
-      return result
+        result= min(max(self.lower_bound, result), self.upper_bound)
     except ValueError as ex:
       if value == '' or value == 'None':
-        return self.middle
+        result= self.middle
       else:
         raise ex
+    return "%g" % round(result + 1e-9, 5) # Prevent negative zero.
 
-  def ignore(self):
-    if self.is_restoring:
-      raise Exception("cannot ignore when restoring")
-    self.minimum= self.maximum= self.divisor= 0
+class MinMaxIgnored:
+  def __init__(self):
+    pass
 
-  def use_clamping(self):
-    if not self.is_restoring:
-      raise Exception("cannot clamp when saving")
-    self.wants_clamping= True
+  def __str__(self):
+    return ''
 
-  def use_negative_one(self):
-    if self.is_restoring:
-      raise Exception("cannot change lower bound when restoring")
-    self.middle= .0
-    self.divisor /= 2
-    self.minimum= self.maximum - self.divisor
+  def add(self, value):
+    pass
+
+  def adjust(self, value):
+    return value
 
 class MinMaxDict:
-  def __init__(self):
+  def __init__(self, columns_to_ignore, using_negative_one, using_standard_deviation):
     self.d= {}
+    self.columns_to_ignore= columns_to_ignore
+    self.using_negative_one= using_negative_one
+    self.using_standard_deviation= using_standard_deviation
 
   def __contains__(self, key):
-    return int(key) in self.d
+    return key in self.d
 
   def __getitem__(self, key):
-    return self.d[int(key)]
+    value= self.d.get(key)
+    if not value:
+      self.d[key]= value= MinMaxIgnored() if key in self.columns_to_ignore else MinMax(self.using_negative_one, self.using_standard_deviation)
+    return value
 
   def __iter__(self):
     return iter(self.d)
-
-  def ignore(self, columns_to_ignore):
-    for k in columns_to_ignore:
-      self.d[k].ignore()
-
-  def update(self, key, value):
-    key= int(key)
-    if key in self.d:
-      self.d[key].add(value)
-    else:
-      self.d[key]= MinMax(value)
-
-  def use_negative_one(self):
-    for v in self.d.values():
-      v.use_negative_one()
 
 def make_seekable(input_file):
   # Assign the return value to a variable to prevent the garbage collector
@@ -124,45 +121,42 @@ def read_pairs(input_file):
     for k, v in enumerate(line.split(',')):
       yield k, v
 
-def normalize(minmax_dict, input_file, output_file):
+def normalize(min_max_dict, input_file, output_file):
   for line in input_file:
     if ',' in line:
       parts= line.split(',')
-      g= ("%g" % minmax_dict[k].adjust(v) for k, v in enumerate(parts))
+      g= (min_max_dict[k].adjust(v) for k, v in enumerate(parts))
       print(*g, sep=",", file=output_file)
     else:
       print(file=output_file)
 
-def save(settings_file, input_file, output_file, columns_to_ignore, using_negative_one):
+def save(settings_file, input_file, output_file, columns_to_ignore, using_negative_one, using_standard_deviation):
   import csr
-  minmax_dict= MinMaxDict()
+  columns_to_ignore= csr.parse(columns_to_ignore, as_index=True)
+  min_max_dict= MinMaxDict(columns_to_ignore, using_negative_one, using_standard_deviation)
   input_file, input_handle= make_seekable(input_file)
   for k, v in read_pairs(input_file):
-    minmax_dict.update(k, v)
-  if using_negative_one:
-    minmax_dict.use_negative_one()
-  columns_to_ignore= csr.parse(columns_to_ignore, as_index=True) if columns_to_ignore else ()
-  minmax_dict.ignore(columns_to_ignore)
-  for k in minmax_dict:
-    print(k, minmax_dict[k], file=settings_file)
+    min_max_dict[k].add(v)
+  for k in min_max_dict:
+    print(k, min_max_dict[k], sep=':', file=settings_file)
   input_file.seek(0)
-  normalize(minmax_dict, input_file, output_file)
+  normalize(min_max_dict, input_file, output_file)
 
 def restore(settings_file, input_file, output_file, wants_clamping):
-  g= (line.split(None, 1) for line in settings_file)
-  minmax_dict= {int(p[0]): MinMax(p[1]) for p in g}
-  if wants_clamping:
-    for v in minmax_dict.values():
-      v.use_clamping()
-  normalize(minmax_dict, input_file, output_file)
+  g= (line.rstrip().split(':') for line in settings_file)
+  def make_min_max(s):
+    return MinMax(s, wants_clamping) if s else MinMaxIgnored()
+  min_max_dict= {int(p[0]): make_min_max(p[1]) for p in g}
+  normalize(min_max_dict, input_file, output_file)
 
 if __name__ == "__main__":
   parser= argparse.ArgumentParser(description="""Scales a CSV dataset so all
-    features are in [0, 1].""", epilog="""You must specify either save (-s,
-    --save) or restore (-r, --restore).  The restore operation ignores the
-    negative-one and ignore options.  The save operation stores information
-    about those options in the settings file.  Only the restore operation uses
-    the clamp option.""")
+    values are in a specific range (default [0, 1]).""", epilog="""You must
+    specify either save (-s, --save) or restore (-r, --restore).  The restore
+    operation ignores the negative-one, standard-deviation, and ignore options.
+    The save operation stores information about those options in the settings
+    file.  Only the restore operation uses the clamp option.  Specifying the
+    standard-deviation option results in output that may exceed the range.""")
   parser.add_argument("-s", "--save", metavar="settings_file",
     type=argparse.FileType('w'),
     help="the scaling parameter settings save file")
@@ -173,6 +167,8 @@ if __name__ == "__main__":
     help="ensure scaled values are within range")
   parser.add_argument("-n", "--negative-one", action="store_true",
     help="use -1 instead of 0 as the lower bound")
+  parser.add_argument("-d", "--standard-deviation", action="store_true",
+    help="use the standard deviation to determine the scaling")
   parser.add_argument("-i", "--ignore", metavar="COLUMNS", default="",
     help="a comma-delimited list of columns to not scale")
   parser.add_argument("input_file", nargs="?",
@@ -183,7 +179,7 @@ if __name__ == "__main__":
     help="the scaled output data file (default standard output)")
   args= parser.parse_args()
   if args.save and not args.restore:
-    save(args.save, args.input_file, args.output_file, args.ignore, args.negative_one)
+    save(args.save, args.input_file, args.output_file, args.ignore, args.negative_one, args.standard_deviation)
   elif args.restore and not args.save:
     restore(args.restore, args.input_file, args.output_file, args.clamp)
   else:
